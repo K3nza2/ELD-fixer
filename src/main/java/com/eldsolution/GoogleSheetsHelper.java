006c04a1593ad9653fea468fccbf79ac7ce1f8c3
Janko Jakovljevic <jankokg2002@gmail.com>
2026-05-12 23:37:15 -0500

    first stable version of ELD fixer

---
+++ src/main/java/com/eldsolution/GoogleSheetsHelper.java
@@ -0,0 +1,43 @@
+package com.eldsolution;
+
+import java.io.FileInputStream;
+import java.util.Collections;
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+
+public class GoogleSheetsHelper {
+    private Sheets sheetsService;
+    private String spreadsheetId;
+    private String range;
+
+    public GoogleSheetsHelper(String credentialsPath) throws Exception {
+        GoogleCredentials credentials = GoogleCredentials
+            .fromStream(new FileInputStream(credentialsPath))
+            .createScoped(Collections.singleton("https://www.googleapis.com/auth/spreadsheets"));
+
+        this.sheetsService = new Sheets.Builder(
+            GoogleNetHttpTransport.newTrustedTransport(),
+            GsonFactory.getDefaultInstance(),
+            new HttpCredentialsAdapter(credentials)
+        )
+        .setApplicationName("Slackbot")
+        .build();
+    }
+    public void SetSpreadsheet (String SpreadSheetId, String range){
+        this.spreadsheetId = SpreadSheetId;
+        this.range = range;
+    }
+    public Sheets getSheetService() {
+        return sheetsService;
+    }
+    public String getSpreadSheetId (){
+        return spreadsheetId;
+    }
+    public String getRange() {
+        return range;
+    }
+}
