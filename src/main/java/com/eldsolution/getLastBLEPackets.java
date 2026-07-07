ac26273961c7c1902b1408fab87031005db8d46d
kenza <jankokg2002@gmail.com>
2026-07-07 07:58:33 -0500

    Added customer id and name search, need to add instances to check for valid packets

--- src/main/java/com/eldsolution/getLastBLEPackets.java
+++ src/main/java/com/eldsolution/getLastBLEPackets.java
@@ -42,37 +42,51 @@ public class getLastBLEPackets {
             .GET()
             .build();
         HttpResponse<String> response1 = client.send(request1,HttpResponse.BodyHandlers.ofString());
-        System.out.println(response1.body());
+        System.out.println(response1.statusCode());
         String ELD = "87A041770089";
         Instant end_instance = Instant.now();
-        Instant begin_instance = end_instance.minus(Duration.ofDays(2));
-        String Begin = begin_instance.toString(); //2026-05-25T04:59:59.999Z
+        Instant begin_instance = end_instance.minus(Duration.ofDays(2)); // od trenutka kada se pozove funkcija pa minus dva dana
+        String Begin = begin_instance.toString(); //2026-05-25T04:59:59.999Z je format
         String end = end_instance.toString();
         String TZO = "-300";
         String Graph = "OBDOdometer";
         String Origin = "siteOperationsManagerDeviceanalysisPackets";
 
+        HttpRequest get_device_info = HttpRequest.newBuilder()
+            .uri(URI.create(url + "proxy/devicehealthoperations/getdevicehealthinfo?deviceSerialNumber=" + ELD))
+            .header("Content-Type", "application/json")
+            .header("Cookie", cookie)
+            .GET()
+            .build();
+        HttpResponse<String> device_health_info = client.send(get_device_info, HttpResponse.BodyHandlers.ofString());
+        String body_device_health_info = device_health_info.body();
+        ObjectMapper device_health_info_mapper = new ObjectMapper();
+        JsonNode device_health_json = device_health_info_mapper.readTree(body_device_health_info);
+        String customerID = device_health_json.path("Data").path("CustomerId").asText("null");
+        String customerName = device_health_json.path("Data").path("CustomerName").asText("null");
+        System.out.println(customerID);
+        System.out.println(customerName);
+        
+ 
         HttpRequest get_anaview = HttpRequest.newBuilder()
             .uri(URI.create(url+"proxy/devicehealthoperations/getanalysisview"))
             .header("Content-type", "application/x-www-form-urlencoded")
             .header("Cookie", cookie)
             .POST(HttpRequest.BodyPublishers.ofString("SerialNumber="+ELD+"&Begin="+Begin+"&End="+end+"&TZOffset="+TZO+"&GraphColumn="+Graph+"&Origin="+Origin))
             .build();
-            //UNFINISHED
         HttpResponse<String> anaresponse = client.send(get_anaview,HttpResponse.BodyHandlers.ofString());
         String body = anaresponse.body();
         ObjectMapper mapper = new ObjectMapper();
         JsonNode json = mapper.readTree(body);
         //JsonNode tabledata = json.get("Data").get("TableData").get(0); newest packet
         JsonNode tabledata = json.get("Data").get("TableData");
-        //Dodati logiku da se nadje customer name!!!!!
         
-        System.out.println(tabledata); // i need Message Reason, Bluetooth Firmware Version and BLE Client
         String BLEClient = tabledata.get(0).path("RowData").path("BLEClient").asText("null");//vreca vrednost koja nije null ako uredjaj ima BLE
         System.out.println(BLEClient);
-        String MessageReason = tabledata.path("RowData").path("MessageReason").asText("null");
+        String MessageReason = tabledata.get(0).path("RowData").path("MessageReason").asText("null");
         System.out.println(MessageReason);
-        String BLEFirmware = tabledata.path("RowData").path("BluetoothFirmwareVersion").asText("null");
+        String BLEFirmware = tabledata.get(0).path("RowData").path("BluetoothFirmwareVersion").asText("null");
         System.out.println(BLEFirmware);
+        //dodati logiku da se ide kroz iteracije paketa ako paket nije ON PERIODIC
     }
 }
